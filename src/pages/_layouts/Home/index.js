58ec0f2a44ecb0eadb17e2fbe5e03af8137e7d00
Ajay Mehta <ajay@atlogys.com>
2022-04-21 22:44:09 +0530

    removed code

--- src/pages/_layouts/Home/index.js
+++ src/pages/_layouts/Home/index.js
@@ -23,14 +23,7 @@ const Home = ({ children }) => {
 
                 <div id="content" className={!isNavbarVisible ? "active" : ""}>
                     {process.env.NODE_ENV === "production" && <div className="user-notification">
-                        If you like my work, please support by{" "}
-                        <a href="https://www.youtube.com/channel/UCdItDI6oTgPW7l9WOJI7ItA/?sub_confirmation=1">
-                            🔔 subscribing to my youtube channel
-                        </a>{" "}
-                        and give a{" "}
-                        <a href="https://github.com/codegeous/react-component-depot">
-                            ⭐ star on github
-                        </a>
+                       
                     </div>}
 
                     {children}
